f74c4e050ded37a9130453dc3592960ec57df3af
MICHELE WANG <michelewang@college.harvard.edu>
2018-04-26 16:23:28 -0400

    added tiles/modal to showcase project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from 'react-modal';
+import CustomModal from './CustomModal';
+import spotify from './spotify.png'
+import berg from './berg.jpg'
+
+  class Tile extends Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+          name: this.props.name,
+          image: this.props.image,
+          description: this.props.description,
+          languages: this.props.languages
+        }
+    }
+    onOpenModal() {
+        this.setState({open: this.props.openModal});
+    }
+    onCloseModal()
+    {
+        this.setState({open:false});
+    }
+    render ()
+    {
+      return (
+        <CustomModal title={this.props.title}
+                     description={this.props.description}
+                     languages={this.props.languages}/>
+
+      );
+    }
+  }
+
+  // class Modal extends React.Component {
 
 class App extends Component {
   render() {
@@ -16,11 +51,20 @@ class App extends Component {
           <p> Studying Applied Math with a focus in Computer Science </p>
           <p> michelewang@college.harvard.edu </p>
           <a href = 'https://github.com/michelewang' class = "trans">
-            <img src = "github.jpg"></img>
           </a>
         </p>
+        <div className='flex-container'>
+          <Tile title="Berg Buddies"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                languages="language 1, language 2" />
+          <Tile title="Spotify Classifier"
+                description="whose manzzza"
+                languages="language 1, language 2" />
+          <Tile title="OCaml Interpreter"
+                description="whose manzzza"
+                languages="language 1, language 2" />
+        </div>
       </div>
-
     );
   }
 }
